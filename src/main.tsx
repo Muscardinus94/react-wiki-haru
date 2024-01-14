37c18cacc33f02b4e457a33c373aec3de652fcf5
Jae Kim <9028364@gmail.com>
2024-01-15 00:17:45 +0900

    chore: Speed Insight Test

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './reset.css';
 import './index.css';
 
