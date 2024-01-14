91570ec9a01bffe046cf1888d9a04f3aa6d6e0b5
Jae Kim <9028364@gmail.com>
2024-01-15 00:17:00 +0900

    chore: Speed Insight Test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './reset.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
