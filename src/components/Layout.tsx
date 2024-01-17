86c5139765288bb0449f8a2cf0c38edc2530b8a4
Jae Kim <9028364@gmail.com>
2024-01-18 00:41:20 +0900

    feat: path 이동시 scroll top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import styles from './Layout.module.css';
 import { Header } from './Header.tsx';
 import { Footer } from './Footer.tsx';
 
 export const Layout = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }, [location.pathname]);
+
   return (
     <div className={styles.container}>
       <Header />
